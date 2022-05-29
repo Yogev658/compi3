adb5bdc6b90e6a2c8081e07427f28edbceb736f7
Anna <38730348+AnnaNovin@users.noreply.github.com>
2022-05-29 19:27:33 +0300

    added Element.h (not complete)

--- Element.h
+++ Element.h
@@ -7,27 +7,20 @@ using std::string;
 using std::cout;
 using std::endl;
 
-
 enum TypeName {VOID, INT, BOOL, STRING, BYTE};
-
-class Type {
-public:
-    TypeName name;
-    bool is_func;
-    bool is_auto;  #TODO needed?
-    vector<Type> *args;
-    Type returnArg;
-    Type(TypeName name, bool is_var, bool is_func, vector<Type> *args, Type returnArg);
-};
 
 class Element {
 public:
-    string name;
-    Element();
-    template <type T> T* to();
+    TypeName typeName;
+    bool is_func;
+    bool is_auto;  #TODO needed?
+    vector<Element> *args;
+    Element returnArg;
+    Element(TypeName name, bool is_func, bool is_auto, vector<Element> *args, Element returnArg);
+    template <Element T> T* to();
 };
 
-template <type T> T *Element::to() {
+template <Element T> T *Element::to() {
     T* e = dynamic_cast<T*>(this);
     if (e == nullptr) {
        cout << "Wasn't able to cast from *" << typeid(this).name() << "to *" << typeid(T).name() <<endl; #TODO Change!
@@ -36,30 +29,15 @@ template <type T> T *Element::to() {
 };
 
 struct Identifier: public  Element {
-    string name;
+    string IdName;
+    Element* type;
     int offset;
-    Type* type;
-    Identifier(string name, int offset, Type* type);
-};
-
-struct Funcs: public Element {
-    Type type;
-    Funcs(Type type);
+    Identifier(string IdName, Element* type, int offset);
 };
 
 struct FuncDecl: public Element {
-    Type type;
-    FuncDecl(Type type);   #TODO how to pass that is_func == 1???
-};
-
-struct RetType: public Element {
-    Type type;
-    RetType(Type type);
-};
-
-struct Formals: public Element {
-    Type type;
-    Formals(Type type);
+    Element element;
+    FuncDecl(Element element);
 };
 
 struct FormalsList: public Element {  #TODO
