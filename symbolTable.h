43b2871eac72e2e7b9373fbc09fe08aee7c03328
Yogev Ron <49362652+Yogev658@users.noreply.github.com>
2022-05-29 13:28:44 +0300

    add symboltable

--- symbolTable.h
+++ symbolTable.h
@@ -5,8 +5,50 @@
 #ifndef COMPI3_SYMBOLTABLE_H
 #define COMPI3_SYMBOLTABLE_H
 
+#include <stack>
+#include <vector>
+
+#include "hw3_output.hpp"
+
+using namespace std;
+class Identifier;
+
+struct Scope{
+    vector<Identifier*>* _elements;
+    int _offset;
+
+    Scope(int offset);
+    ~Scope();
+
+    void insert(Identifier* id);
+    Identifier* find(string idName);
+
+};
 
 class symbolTable {
+    stack<Scope&> _scopesStack;
+
+public:
+    SymbolTable(): _scopeStack(){}
+    ~SymbolTable(){}
+
+void SymbolTable::pushScope(){
+    assert(_scopeStack.size() != 0);
+    int offset = _scopesStack->top()->offset + _scopesStack->top()->size();
+    this->_scopeStack(Scope(offset));
+}
+    void addScope();
+
+    void insert();
+    Identifier* find();
+    void removeScope();
+
+    void addFunctionScope();
+    void removeFunctionScope();
+
+
+
+
 
 };
 
